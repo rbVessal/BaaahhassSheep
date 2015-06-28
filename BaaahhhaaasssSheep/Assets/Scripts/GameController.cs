c0547406ae19a176ba4b79eff9bb1168379e94a2
Ellie <Ellie@Ellies-MacBook-Pro.local>
2015-06-28 05:36:05 -0700

    gui stuff

---
+++ BaaahhhaaasssSheep/Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//game controller class for NightmareWarrior
+//contact: jordanjalles at gmail dot com
+
+
+/*if iterated, this class will need to break into
+ *two classes, the GameController class:
+ *		start - startscreen - stop - (un)pause - gameOver - update
+ * 
+ * and a Level class:
+ * 		boss - enemyController - player - stopAll - startAll
+ * 
+ * for this prototype we are keeping this class as one for simplicities sake
+ * 
+ * */
+
+public class GameController : MonoBehaviour {
+
+
+	public Canvas startScreen;
+
+	string state = "StartScreen";
+
+	GameObject player;
+	GameObject enemyController;
+	GameObject boss;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update() {
+		if (state == "StartScreen") {
+			if (Input.GetKey(KeyCode.Space)){
+				StartGame ();
+			}
+		}
+
+		if (state == "Playing") {
+			if (Input.GetKey(KeyCode.P)) {
+				PauseGame ();
+			}
+		}
+		if (state == "Paused") {
+			if (Input.GetKey(KeyCode.P)) {
+				UnPauseGame ();
+			}
+		}
+	}
+	
+	void StartGame(){
+		state = "Playing";
+		//remove gui and intro screen
+		//spawn sheep and world
+		//start the timer
+	}
+	
+	void PauseGame(){
+		state = "Paused";
+		StopAll ();
+
+	}
+
+	void UnPauseGame(){
+		state = "Playing";
+		StartAll();
+	}
+
+	void GameOver(){
+		state = "GameOver";
+		StopAll ();
+
+	}
+
+	void StartScreen(){
+		//display gui
+		state = "StartScreen";
+	}
+
+	void StopAll(){
+		//tell Baahdass to stop moving
+		//tell wolves to stop moving
+		//tell werewolve to stop moving
+
+	}
+
+	void StartAll(){
+		//tell Baahdass to start moving
+		//tell wolves to start moving
+		//tell werewolve to start moving
+		
+	}
+
+}
---
+++ BaaahhhaaasssSheep/Assets/Scripts/wolfScript.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wolfScript : MonoBehaviour 
+{
+
+	GameObject player;
+	float DISTANCE_THRESHOLD_TO_PLAYER = 20f;
+	Vector2 runningVelocity = new Vector2(-20, 0);
+	string state = "idle";
+	Rigidbody2D rigidbody;
+	BoxCollider2D collider;
+	bool collisionOccurred = false;
+	bool firstCollisionDidOccur = false;
+	Camera camera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		rigidbody = GetComponent<Rigidbody2D>();
+		collider = GetComponent<BoxCollider2D>();
+		camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (distanceToPlayer() < DISTANCE_THRESHOLD_TO_PLAYER)
+		{
+			if(!collisionOccurred)
+			{
+				run();
+			}
+			else
+			{
+				runAway();
+			}
+			GetComponent<Animator>().SetInteger("state", 1);
+		}
+		else
+		{
+			if(!collisionOccurred)
+			{
+				if(firstCollisionDidOccur)
+				{
+					run ();
+				}
+			}
+			else
+			{
+				runAway();
+			}
+		}
+	}
+
+	void run()
+	{
+		rigidbody.velocity = runningVelocity;
+	}
+
+	void runAway()
+	{
+//			float rightSidePositionX = (camera.pixelWidth + (collider.size.x/2));
+//			float runAwayVelocityX = ((rigidbody.velocity.x + runningVelocity.x + 
+//			                           rigidbody.position.x + (collider.size.x/2)));
+		float rightSidePositionX = (collider.size.x/2) + rigidbody.position.x;
+		Debug.Log ("right: " + rigidbody.position.x);
+		Debug.Log ("right side: " + rightSidePositionX);
+		//			Debug.Log ("runaway velocity x: " + runAwayVelocityX);
+		if(rightSidePositionX > 50)
+		{
+			this.transform.localScale = new Vector3(this.transform.localScale.x * -1, 
+			                                          this.transform.localScale.y, 
+			                                          this.transform.localScale.z);				
+			collisionOccurred = false;
+			Debug.Log("reset");
+		}
+		else
+		{
+			rigidbody.velocity = new Vector2(-runningVelocity.x, 0);
+		}
+	}
+
+	float distanceToPlayer()
+	{
+		return this.transform.position.x - player.transform.position.x ;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.gameObject.tag == "Player")
+		{
+//			rigidbody.velocity = new Vector2(-collision.relativeVelocity.x, 
+//			                                 0);
+			collisionOccurred = true;
+			firstCollisionDidOccur = true;
+			this.transform.localScale = new Vector3(this.transform.localScale.x * -1, 
+			                                          this.transform.localScale.y, 
+			                                          this.transform.localScale.z);
+		}
+	}
+}
